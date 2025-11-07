6bbdc196582e37016d5202a295090cb88fb389d1
Vince1993 <52530147+Vince1993@users.noreply.github.com>
2025-11-07 09:56:24 +0100

    progress10

--- js.js
+++ js.js
@@ -4,11 +4,26 @@ var myFeature = {
         $("#respo_menu").click(this.openNav);
         this.loadImages();
 
-       
+        $(".gallery").on("click", "img", (event) => {
+            const image = event.currentTarget; // The clicked <img> element
+            this.openFullscreen(image);
+          });
+
+        $("#fullscreenImg").click(this.closeFullscreen);
+         
+        // Hide image when fullscreen exits
+        $(document).on("fullscreenchange webkitfullscreenchange msfullscreenchange", function () {
+            if (!document.fullscreenElement &&
+                !document.webkitFullscreenElement &&
+                !document.msFullscreenElement) {
+                $("#fullscreenImg").hide();
+            }
+        });
     },
     openNav: function () {
         $("header nav").slideToggle();
     },
+
     loadImages: function () {
         $.getJSON('images.json', function (images) {
             $.each(images, function (index, image) {
@@ -16,69 +31,50 @@ var myFeature = {
                 $('.gallery').append(`<img src="media/compressed/${image}" data-target="media/${newSrc}" alt="One sketch shown on website">`);
             });
         });
+    },
+
+    openFullscreen: function (image) {
+        {
+            let img_src = $(image).attr('src');
+            let img_file_name = img_src.split('/').pop();
+            let newFileName = img_file_name.replace('.jpg', '.png');
+            let new_src = "media/" + newFileName;
+
+            let $fullImg = $("#fullscreenImg");
+            $fullImg.attr('src', new_src);
+            $fullImg.show("full");
+            let elem = $fullImg[0]; // Get the raw DOM element
+
+            // Check if currently in fullscreen
+            let isFullscreen = document.fullscreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+
+            if (!isFullscreen) 
+            {
+                if (elem.requestFullscreen) {elem.requestFullscreen();} 
+                else if (elem.webkitRequestFullscreen) {elem.webkitRequestFullscreen();} 
+                else if (elem.msRequestFullscreen) {elem.msRequestFullscreen();}
+            } 
+            if(isFullscreen)
+            {
+
+            if (document.exitFullscreen) { document.exitFullscreen(); }
+            else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); }
+            else if (document.msExitFullscreen) { document.msExitFullscreen(); }
+    
+        }   
+        }
+    },
+
+    closeFullscreen : function(){
+        
+            if (document.exitFullscreen) { document.exitFullscreen(); }
+            else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); }
+            else if (document.msExitFullscreen) { document.msExitFullscreen(); }
+        
+            $("#fullscreenImg").hide();
     }
+
 };
 
 $(document).ready(function () {
-    myFeature.init();
-    $('.gallery').on('click', 'img', function () {
-        const fullSrc = $(this).data('target');
-
-        // Create full-screen image element
-        const $fullImg = $('<img>', {
-            src: fullSrc,
-            css: {
-                width: '100%',
-                height: '100%',
-                objectFit: 'contain',
-                backgroundColor: '#06021dff',
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                zIndex: 9999
-            }
-        });
-
-        // Append to body
-        $('body').append($fullImg);
-
-        // Request fullscreen
-        const elem = $fullImg[0];
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) {
-            elem.webkitRequestFullscreen();
-        } else if (elem.mozRequestFullScreen) {
-            elem.mozRequestFullScreen();
-        } else if (elem.msRequestFullscreen) {
-            elem.msRequestFullscreen();
-        }
-
-        // Remove image when exiting fullscreen
-        $(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function () {
-            if (!document.fullscreenElement &&
-                !document.webkitFullscreenElement &&
-                !document.mozFullScreenElement &&
-                !document.msFullscreenElement) {
-                $fullImg.remove();
-            }
-        });
-
-        $fullImg.on('click', function () {
-            if (document.fullscreenElement ||
-                document.webkitFullscreenElement ||
-                document.mozFullScreenElement ||
-                document.msFullscreenElement) {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.webkitExitFullscreen) {
-                    document.webkitExitFullscreen();
-                } else if (document.mozCancelFullScreen) {
-                    document.mozCancelFullScreen();
-                } else if (document.msExitFullscreen) {
-                    document.msExitFullscreen();
-                }
-            }
-        });
-    });
-});
+    myFeature.init();});
