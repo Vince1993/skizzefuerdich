a28771676a4136dc9f5afe10cef3dc53e1f87a72
Vince1993 <52530147+Vince1993@users.noreply.github.com>
2025-11-21 06:35:23 +0100

    refactored code and colors

--- javaScript.js
+++ javaScript.js
@@ -1,37 +1,43 @@
 $(document).ready(function () {
     $("#respo").click(slideInAndOut);
 
-    for (let i = 0; i <= 16; i++) {
-        $('.img_grid').append(`<div class="img_wrap">
-            <img src="img_small/sketch${i}.jpg" id="img${i}">
-            <div class="overlay-number">${i+1}</div>
-          </div>`);
-    }
+    loadImages();
 
-    $(".img_grid img").on("click", function () {
-        if (!document.fullscreenElement) {
-            // Enter fullscreen
-            this.requestFullscreen();
-        } else {
-            // Exit fullscreen
-            document.exitFullscreen();
-        }
-    });
+    $(".img_grid img").on("click", openCloseFullScreen);
 
-    $("a[href^='#']").click(function (e) {
-        e.preventDefault(); // prevent default jump
-        var target = $($(this).attr("href"));
-        if (target.length) {
-            $("html, body").animate({
-                scrollTop: target.offset().top
-            }, 200); // smooth scroll in 800ms
-        }
-    });
+
+    $("a[href^='#']").on("click", scrollingSmoothToId);
 });
 
 function slideInAndOut() {
     $("nav a").slideToggle(1000);}
 
+function loadImages() {
+    for (let i = 0; i <= 16; i++) {
+        $('.img_grid').append(`<div class="img_wrap">
+            <img src="img_small/sketch${i}.jpg" id="img${i}">
+            <div class="overlay-number">${i + 1}</div>
+            </div>`);}}
+
+function openCloseFullScreen(){
+    if (!document.fullscreenElement) {
+        // Enter fullscreen
+        this.requestFullscreen();}
+    else {
+        // Exit fullscreen
+        document.exitFullscreen();}
+}
+
+function scrollingSmoothToId(event) {
+    event.preventDefault(); // prevent default jump
+    var target = $($(this).attr("href"));
+    if (target.length) {
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 400); // smooth scroll in 400ms
+    }
+}
+    
 
 
 
